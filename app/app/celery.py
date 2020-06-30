a06a235200ffeea8fe345dbd488675d00f284e90
key kim <keyhyuk.kim@gmail.com>
2020-06-30 19:23:12 +0900

    Initalize project

---
+++ app/app/celery.py
@@ -0,0 +1,6 @@
+import os
+from celery import Celery
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+app = Celery('celery_app')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
---
+++ app/board_app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.ForeignKey('board_app.Url', on_delete=models.CASCADE, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = 'Posts'
+
+class Url(models.Model):
+    url = models.URLField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.url[:50]
+
+    class Meta:
+        verbose_name_plural = 'Url'
