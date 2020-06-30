3247c82e6c96861a586b44531446f0a121fa7908
key kim <keyhyuk.kim@gmail.com>
2020-07-01 08:57:44 +0900

    Add default crawler

---
+++ crawler_app/crawler_app/spiders/hallym_software_spider.py
@@ -0,0 +1,48 @@
+import scrapy
+import time
+from scrapy.spiders import CrawlSpider, Rule
+import logging
+from datetime import datetime
+from ..items import CrawlerAppItem
+from scrapy import signals
+from scrapy import Spider
+class HallymSoftwareSpider(CrawlSpider):
+    root_url = 'https://sw.hallym.ac.kr/'
+    logger = logging.getLogger(__name__)
+    name = 'HallymSoftwareSpider'
+
+    def start_requests(self):
+        urls = [
+            'https://sw.hallym.ac.kr/index.php?mp=5_1'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse_item)
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(HallymSoftwareSpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
+        crawler.signals.connect(spider.spider_opened, signal=signals.spider_opened)
+        return spider
+
+
+    def spider_opened(self, spider):
+        spider.logger.info('Hallym Spider opened: %s', spider.name)
+
+    def spider_closed(self, spider):
+        spider.logger.info('Hallym Spider closed: %s', spider.name)
+    
+
+    def parse_item(self, response):
+        raw_url = response.xpath('/html/body/div/div[3]/div[2]/div[2]/table/tbody/tr/td[2]/a/@href').extract()
+        raw_title = response.xpath("/html/body/div/div[3]/div[2]/div[2]/table/tbody/tr/td[2]/a/@title").extract()
+        item = CrawlerAppItem()
+        item['log'] = [
+            {
+                'url': self.root_url + url,
+                'title': raw_title[idx]
+            } for idx, url in enumerate(raw_url)
+        ]
+        item['timestamp'] = datetime.now().strftime("%H:%M:%S")
+
+        yield item
