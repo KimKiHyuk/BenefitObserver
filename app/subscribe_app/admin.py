b5a26897157537e3c3d726cefb15d4a7e79514a3
key kim <keyhyuk.kim@gmail.com>
2020-07-10 19:22:15 +0900

    Make Auth <-> subscribe model

--- app/subscribe_app/admin.py
+++ app/subscribe_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Subscribe
+from .models import *
 # Register your models here.
 admin.site.register(Subscribe)
+admin.site.register(Auth_Subscribe)
--- app/subscribe_app/models.py
+++ app/subscribe_app/models.py
@@ -8,16 +8,23 @@ class Subscribe(models.Model):
         NSDEPT = 'Natural science', _('자연과학대학')
 
     objects = models.Manager()
-    user = models.ForeignKey("auth_app.User", on_delete=models.CASCADE, blank=False)
     topic = models.CharField(
         max_length=30,
         choices=Topic.choices,
         default=Topic.SWDEPT,
     )
+
+    def __str__(self):
+        return str(self.topic)
+
+class Auth_Subscribe(models.Model):
+    objects = models.Manager()
+    user = models.ForeignKey("auth_app.User", on_delete=models.CASCADE, blank=False)
+    subscribe = models.ForeignKey("Subscribe", on_delete=models.CASCADE, blank=False)
     
 
     def __str__(self):
-        return str(f'{self.user} | {self.topic}')
+        return str(f'{self.user} | {self.subscribe}')
     
     class Meta:
-        verbose_name_plural = 'Subscribe'
+        verbose_name_plural = 'Auth_Subscribe'
