9c2408aa86bc4548c61b3c14c5a5cd18ec210fcf
key kim <keyhyuk.kim@gmail.com>
2020-07-19 18:10:47 +0900

    Link async task using celery

--- app/celery_app/tasks.py
+++ app/celery_app/tasks.py
@@ -10,16 +10,19 @@ from celery.utils.log import get_task_logger
 from django.utils.dateparse import parse_date
 log1 = get_task_logger(__name__)
 
+queue_subscribe_table = {
+    'sw' : 1,
+    'ns' : 2
+}
 @app.task
 def push_fcm(**kwargs):
     from subscribe_app.models import Auth_Subscribe
-    _all = Auth_Subscribe.objects.filter(kwargs[''])
-
-    print(kwargs)
-    print(_all)
+    _all = Auth_Subscribe.objects.filter(subscribe_id=kwargs['queue'])
+    print('hi!', _all)
 
 @task(name='celery_app.crawler.sw')
 def sw(**kwargs):
+    is_created = False
     from celery_app.models import CrawlerTask
     from board_app.models import Posts, Url
 
@@ -37,4 +40,10 @@ def sw(**kwargs):
         )
 
         if created:
-            push_fcm.delay(kwargs={'hi':'hello'})
+            is_created = True
+    
+
+    
+    if is_created:
+        if post['url'].find('sw.hallym.ac.kr') != -1:
+            push_fcm.delay(queue=queue_subscribe_table['sw'])
